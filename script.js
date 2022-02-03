c8648dfbf8c32c73f9880acf82999684af9c7c2f
Mehmet Güdük <mehmetguduk98@gmail.com>
2022-02-03 12:59:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let input_followers = document.querySelector("#input-followers");
 let input_following = document.querySelector("#input-following");
 let unfollowers_list = document.querySelector(".unfollowers");
 let result_table = document.querySelector(".result-table");
+let card_header = document.querySelector(".card-header");
 
 document.getElementById("submit").addEventListener("click", function () {
 
@@ -18,35 +19,44 @@ document.getElementById("submit").addEventListener("click", function () {
 
         hidden_follower.innerHTML = `${input_followers.value}`
         hidden_following.innerHTML = `${input_following.value}`
-        let following_data = document.querySelectorAll("#following .E1N1ByPFWo4AJLHovIBQ");
-        let follower_data = document.querySelectorAll("#follower .E1N1ByPFWo4AJLHovIBQ");
+
+        let following_data = document.querySelectorAll("#following .XiVwj5uoqqSFpS4cYOC6");
+        let follower_data = document.querySelectorAll("#follower .XiVwj5uoqqSFpS4cYOC6");
 
         following_data.forEach(u => {
-            let name = u.firstElementChild.textContent.trim();
-            let type = u.lastElementChild.textContent.trim();
-            let user = { name: name, type: type }
+            let name = u.children[1].querySelector(".nk6UgB4GUYNoAcPtAQaG").textContent;
+            let type = u.children[1].querySelector(".Za_uNH8nTZ0qCuIqbPLZ").textContent;
+            let url = "https://open.spotify.com" + u.children[1].querySelector("a").getAttribute("href");
+            let img;
+            try { img = u.children[0].querySelector("img").getAttribute("src") }
+            catch (err) {
+                img = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/OOjs_UI_icon_userAvatar-invert.svg/1024px-OOjs_UI_icon_userAvatar-invert.svg.png"
+            }
+            let user = { name: name, type: type, url: url, img: img }
             following_list.push(user);
         })
 
         follower_data.forEach(u => {
-            let name = u.firstElementChild.textContent.trim();
+            let name = u.children[1].querySelector(".nk6UgB4GUYNoAcPtAQaG").textContent;
             follower_list.push(name);
         })
 
         following_list.forEach(u => {
-            if (u.type === "Profil" && follower_list.includes(u.name) === false) {
-                unfollowers.push(u.name)
+            if (follower_list.includes(u.name) === false) {
+                unfollowers.push(u)
             }
         })
 
         unfollowers.forEach(unfollower => {
-            unfollowers_list.innerHTML += `<li class="unfollower">${unfollower}</li>`
+            unfollowers_list.innerHTML += `<li class="unfollower"><img src="${unfollower.img}"><a href="${unfollower.url}">${unfollower.name}</a>- ${unfollower.type}</li>`
         })
 
-        if (follower_list.length === 0 && following_list.length === 0) { 
+        if (follower_list.length === 0 && following_list.length === 0) {
+            card_header.textContent = `Unfollowers`
             result_table.style.display = "none";
         }
-        else{
+        else {
+            card_header.textContent = `${unfollowers.length} Unfollowers Listed`
             result_table.style.display = "block";
         }
 
@@ -55,7 +65,7 @@ document.getElementById("submit").addEventListener("click", function () {
         input_followers.value = "";
         input_following.value = "";
     }
-    else{
+    else {
         result_table.style.display = "none";
     }
 
