ef8e6fdfc4893433fbfb48367ab2a75bc5620734
mehmetguduk <mehmetguduk98@gmail.com>
2024-03-23 16:50:18 +0300

    master

--- script.js
+++ script.js
@@ -29,20 +29,19 @@ document.querySelector(".fa-search").addEventListener("click", function () {
         let follower_data = document.querySelectorAll("#follower .XiVwj5uoqqSFpS4cYOC6");
 
         following_data.forEach(u => {
-            let name = u.children[1].querySelector(".nk6UgB4GUYNoAcPtAQaG").textContent;
-            let type = u.children[1].querySelector(".Za_uNH8nTZ0qCuIqbPLZ").textContent;
-            let url = "https://open.spotify.com" + u.children[1].querySelector("a").getAttribute("href");
+            let name = u.querySelector(".bFhcPb").textContent;
+            let url = "https://open.spotify.com" + u.querySelector("a").getAttribute("href");
             let img;
-            try { img = u.children[0].querySelector("img").getAttribute("src") }
+            try { img = u.querySelector("img").getAttribute("src") }
             catch (err) {
                 img = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/OOjs_UI_icon_userAvatar-invert.svg/1024px-OOjs_UI_icon_userAvatar-invert.svg.png"
             }
-            let user = { name: name, type: type, url: url, img: img }
+            let user = { name: name, url: url, img: img }
             following_list.push(user);
         })
 
         follower_data.forEach(u => {
-            let name = u.children[1].querySelector(".nk6UgB4GUYNoAcPtAQaG").textContent;
+            let name = u.querySelector(".bFhcPb").textContent;
             follower_list.push(name);
         })
 
@@ -53,8 +52,10 @@ document.querySelector(".fa-search").addEventListener("click", function () {
         })
 
         unfollowers.forEach(unfollower => {
-            unfollowers_list.innerHTML += `<li class="unfollower"><img src="${unfollower.img}"><a href="${unfollower.url}">${unfollower.name}</a>- ${unfollower.type}</li>`
+            unfollowers_list.innerHTML += `<li class="unfollower"><img src="${unfollower.img}"><a href="${unfollower.url}">${unfollower.name}</a></li>`
         })
+
+        console.log(unfollowers)
 
         if (follower_list.length === 0 && following_list.length === 0) {
             card_header.textContent = `Unfollowers`
@@ -73,5 +74,4 @@ document.querySelector(".fa-search").addEventListener("click", function () {
     else {
         result_table.style.display = "none";
     }
-
 })
